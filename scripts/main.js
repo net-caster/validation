21570bf1164c063926a47a23a6eb99a67c4aaede
Tyler McGinnsis <mcginnis.tyler@gmail.com>
2019-02-04 18:58:52 +0000

    added obj properties

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
 const patterns = [{
-        username: /^[a-z\d]{4,12}$/i
+        username: /^[a-z\d]{4,12}$/i,
+        isInvalid: function(input) {
+            return input.value.length < 4 || input.value.length > 12;
+        },
+        listItems: document.querySelectorAll('.user-list li:nth-child(1)')
     },
     {
         email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i
@@ -9,7 +13,11 @@ const patterns = [{
     }
 ];
 
-const inputs = document.querySelectorAll('input');
+const validities = [{
+
+}]
+
+const inputFields = document.querySelectorAll('input');
 
 
 const validate = (field, regex) => {
@@ -20,11 +28,23 @@ const validate = (field, regex) => {
     }
 }
 
-inputs.forEach(input => {
-    input.addEventListener('keyup', e => {
-        // console.log(e.target.attributes.name.value);
-        let regPat = patterns.filter(obj => obj[e.target.attributes.name.value]);
-        // console.log(regPat);
-        validate(e.target, regPat[0][e.target.attributes.name.value]);
+const checkInputs = (inputs) => {
+    inputs.forEach(input => {
+        input.addEventListener('keyup', e => {
+            // console.log(e.target.attributes.name.value);
+            let regPat = patterns.filter(obj => obj[e.target.attributes.name.value]);
+            // console.log(regPat);
+            validate(e.target, regPat[0][e.target.attributes.name.value]);
+        });
     });
-});
+}
+
+checkInputs(inputFields);
+
+/* let userObj = patterns.filter(obj => obj.listItems);
+
+userObj[0].listItems.forEach(item => {
+    item.className = 'valid';
+})
+
+console.log(userObj[0].listItem[1]); */
